1ae260d65cd2fbd141e1a08e5433384312710c48
root <root@Debian.myguest.virtualbox.org>
2026-01-19 16:58:04 +0100

    Configure les providers de AWS et de kubernetes

---
+++ terraform/provider.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  
+  default_tags {
+    tags = {
+      Project     = "InfoLine"
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Owner       = "jenkins-leroy"
+    }
+  }
+}
+
+# Provider Kubernetes (sera configuré après la création du cluster EKS)
+provider "kubernetes" {
+  host                   = try(module.eks.cluster_endpoint, "")
+  cluster_ca_certificate = try(base64decode(module.eks.cluster_certificate_authority_data), "")
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      try(module.eks.cluster_name, ""),
+      "--region",
+      var.aws_region
+    ]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = try(module.eks.cluster_endpoint, "")
+    cluster_ca_certificate = try(base64decode(module.eks.cluster_certificate_authority_data), "")
+    
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        try(module.eks.cluster_name, ""),
+        "--region",
+        var.aws_region
+      ]
+    }
+  }
+}
