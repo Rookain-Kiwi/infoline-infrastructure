5209612c1c862da05771f9b1904df4c7c562aea2
root <root@Debian.myguest.virtualbox.org>
2026-01-19 16:30:34 +0100

    rajout des variables du module VPC

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,40 @@
+variable "project_name" {
+  description = "Nom du projet"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environnement de déploiement"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block pour le VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "Liste des availability zones"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks pour les subnets privés"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks pour les subnets publics"
+  type        = list(string)
+}
+
+variable "database_subnet_cidrs" {
+  description = "CIDR blocks pour les subnets de base de données"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags additionnels"
+  type        = map(string)
+  default     = {}
+}
