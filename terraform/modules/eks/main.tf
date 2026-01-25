b9395a6567407d7f0e657f65f687fcd4cf8c5cea
debian <debian@Debian.myguest.virtualbox.org>
2026-01-25 18:49:16 +0100

    from capacity_type by default to [200~capacity_type  = "SPOT"

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -198,8 +198,8 @@ resource "aws_eks_node_group" "main" {
   subnet_ids      = var.private_subnet_ids
 
   instance_types = var.node_instance_types
-  # ON_DEMAND provoque un faux positif lors de la création de l'aws_eks_node_group
-  # capacity_type  = "ON_DEMAND"
+  # ON_DEMAND provoque un faux positif lors de la création de l'aws_eks_node_group: on passe en SPOT
+  capacity_type  = "SPOT"
 
   scaling_config {
     desired_size = var.node_desired_size
