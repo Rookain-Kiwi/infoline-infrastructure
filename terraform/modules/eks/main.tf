e71cce89922599eea6dee8629b0f0e38bd769abd
debian <debian@Debian.myguest.virtualbox.org>
2026-02-06 16:34:40 +0100

    Upgrade to t3.small and add ELK stack monitoring
    - Upgrade EKS nodes from t3.micro to t3.small (2GB RAM) to support ELK stack
    - Enable EBS CSI driver addon via Terraform (previously commented)
    - Add ELK stack Kubernetes manifests:
      - Namespace elk-stack
      - Elasticsearch StatefulSet with 1Gi resources
      - Elasticsearch Service (ClusterIP)
      - Elasticsearch PersistentVolumeClaim (5Gi gp2)
    - Update terraform variables in variables.tf, terraform.tfvars, and terraform.tvars
    
    Justification: t3.micro insufficient for ELK stack deployment (memory + pod limits).
    t3.small provides adequate resources for Elasticsearch + backend/frontend apps.

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -247,14 +247,14 @@ resource "aws_iam_openid_connect_provider" "cluster" {
   )
 }
 
-# Add-on EBS CSI Driver (désactivé - non nécessaire pour ce projet et consomme trop de pods pour un Free Tier)
-# resource "aws_eks_addon" "ebs_csi_driver" {
-#   cluster_name = aws_eks_cluster.main.namecd 
-#   addon_name   = "aws-ebs-csi-driver"
-#   depends_on = [
-#     aws_eks_node_group.main
-#   ]
-# }
+# Add-on EBS CSI Driver
+ resource "aws_eks_addon" "ebs_csi_driver" {
+   cluster_name = aws_eks_cluster.main.namecd 
+   addon_name   = "aws-ebs-csi-driver"
+   depends_on = [
+     aws_eks_node_group.main
+   ]
+ }
 
 # Add-on CoreDNS
 resource "aws_eks_addon" "coredns" {
